5dc2b02f7bc02f99232ee579a99da171b919cbe6
WalterCorsini <corsini.walter@gmail.com>
2024-05-23 23:18:55 +0200

    bonus don't work

--- dddd.php
+++ dddd.php
@@ -1,23 +1,10 @@
-<?php
+
+<?php 
 // ottengo i file dal file json
 $list_dischi_json = file_get_contents("dischi.json");
 
 // converto per utilizzare in php
 $list_php = json_decode($list_dischi_json, true);
-
-
-// aggiungo variabile like per ogni disco in lista che non ha la variabile like
-
-if(isset($_POST['id'])){
-    foreach($list_php as $key => $cur_card){
-        if($key === $_POST['id']){
-            $cur_card['like'] = $like;
-        }
-    }
-
-}
-    file_put_contents("dischi.json", json_encode($list_php));
-
 
 // la salvo in un array associativo
 $array = [
@@ -32,3 +19,4 @@ header("Content-Type: application/json");
 
 // gli invio la risposta
 echo $json_string;
+?>
--- js/script.js
+++ js/script.js
@@ -3,8 +3,40 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            arrayDischi:[],
+            id:"",
+            like:"",
         }
     },
     created(){
+        axios
+        .get("http://localhost/boolean/php-dischi-json/server.php")
+        .then((resp) => {
+        this.arrayDischi = resp.data.results;
+          console.log(resp.data.results);
+        });
+    },
+    methods:{
+      addLike(elem){
+        elem.like = !elem.like;
+        this.id = elem.id;
+        
+        const data = {
+          id: this.id,
+          like: true,
+        };
+        axios.post(
+          "http://localhost/boolean/php-dischi-json/server.php",
+          data,
+          {
+            headers: {
+              "Content-type": "multipart/form-data",
+            },
+          }
+        )
+        .then((resp) => {
+          this.arrayDischi = resp.data.results;
+        });
+      }
     }
-}).mount("#app");
+    }).mount("#app");
